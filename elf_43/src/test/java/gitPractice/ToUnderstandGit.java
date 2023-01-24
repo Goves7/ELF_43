2a3042d6a928317a8183c8dbd6ea703164c3c383
Goves7 <collingoves1996@gmail.com>
2023-01-24 14:58:54 +0530

    Added new Package and Class gitPractice and ToUnderstandGit.java

---
+++ elf_43/src/test/java/gitPractice/ToUnderstandGit.java
@@ -0,0 +1,11 @@
+package gitPractice;
+
+import org.testng.annotations.Test;
+
+public class ToUnderstandGit {
+
+	@Test
+	public void gitConflict() {
+		System.out.println("Tis Is To Check Git Conflict");
+	}
+}
